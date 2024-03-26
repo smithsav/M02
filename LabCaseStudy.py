4b44a84f9236b36ec32a372a13dd8f6033d8591a
Savannah Smith <ssmith1695@ivytech.edu>
2024-03-26 18:04:03 -0500

    addition of comments

--- LabCaseStudy.py
+++ LabCaseStudy.py
@@ -7,21 +7,23 @@ and test if the student qualifies for either the Dean's List or the Honor Roll.
 
 def main():
     while True:
-        last_name = input("Enter student's last name (or 'ZZZ' to quit): ")
+        last_name = input("Enter student's last name (or 'ZZZ' to quit): ") # Asking user for Last Name or to quit the program
         if last_name == 'ZZZ':
             print("Exiting program...")
             break
         
-        first_name = input("Enter student's first name: ")
-        gpa = float(input("Enter student's GPA: "))
+        first_name = input("Enter student's first name: ") # Asking user for First Name
+        gpa = float(input("Enter student's GPA: ")) # Asking user for GPA (float includes number with decimal)
         
-        if gpa >= 3.5:
+        if gpa >= 3.5: # Check if GPA is a 3.5 or above (deans list)
             print(f"{first_name} {last_name} has made the Dean's List.")
-        elif gpa >= 3.25:
+
+        elif gpa >= 3.25: # Check if GPA is a 3.25 or greater (honor roll)
             print(f"{first_name} {last_name} has made the Honor Roll.")
-        else:
+
+        else: # If not greater or equal to 3.5 OR 3.25 (does not qualifu for honors or deans list)
             print(f"{first_name} {last_name} does not qualify for any honors.")
-        print()  # Just for spacing
+        print()  # Just for spacing when restarting 
 
 
 if __name__ == "__main__":
